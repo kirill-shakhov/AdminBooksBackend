582c3659e43e2ae2c502dcd9ee5c54930200c21a
кирилл <shakhov-work@yandex.ru>
2024-01-10 13:14:30 +0300

    master feat(Author.js) added new model Author

---
+++ src/models/Author.js
@@ -0,0 +1,11 @@
+// AdminBooksBackend > src > models > User.js
+const {Schema, model} = require('mongoose');
+
+const Author = new Schema({
+    name: {type: String, required: true},
+    user: { type: Schema.Types.ObjectId, ref: 'User' }
+});
+
+
+module.exports = model('Author', Author);
+
