a6763eaaef3ac7fca9cb7c8ae9a58510b2d14579
кирилл <shakhov-work@yandex.ru>
2024-01-12 16:05:55 +0300

    master feat(bookRouter.js) added new routes

--- src/api/routes/bookRouter.js
+++ src/api/routes/bookRouter.js
@@ -1,16 +1,39 @@
-// AdminBooksBackend > src > api > routes > authRouter.js
-
 const Router = require('express');
-const upload = require('../../config/multerConfig');
-
 const router = new Router();
+
+const upload = require('../../config/multerBookConfig');
+const {check, validationResult} = require("express-validator");
+
 const controller = require('../controllers/bookController');
 const authMiddleware = require('../../middleware/authMiddleware');
-// const roleMiddleware = require('../../middleware/roleMiddleware');
+const validateUploadBookMiddleware = require('../../middleware/validateUploadBookMiddleware');
 
 router.get('/genres', authMiddleware, controller.getGenres);
-router.post('/genres/add', authMiddleware, controller.addGenre);
+router.post('/genres/add',
+    authMiddleware,
+    [check('name', 'Название жанра не может быть пустым').notEmpty()],
+    controller.addGenre);
+router.get('/genre/:genreId', authMiddleware, controller.getBooksByGenre);
+
+router.get('/authors', authMiddleware, controller.getAuthors);
+router.post('/authors/add',
+    authMiddleware,
+    [check('name', 'Название автора не может быть пустым').notEmpty()],
+    controller.addAuthor);
+router.get('/authors/:authorId', authMiddleware, controller.getBooksByAuthor);
+
+router.post('/upload',
+    authMiddleware,
+    upload,
+    validateUploadBookMiddleware,
+    controller.uploadBook);
+router.get('/', authMiddleware, controller.getBooks);
+router.get('/:bookId', authMiddleware, controller.getBook);
+router.patch('/:bookId', authMiddleware, controller.updateBook);
+router.delete('/:bookId', authMiddleware, controller.deleteBook);
 
 
 module.exports = router;
 
+
+//     Если ваше приложение предполагает работу с большим количеством книг, рассмотрите внедрение пагинации и сортировки в методы, которые возвращают списки книг.
