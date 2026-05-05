1faf5922b08518152525432afe18df6b2602bce7
Kirill <checkster98@gmail.com>
2026-05-05 11:20:32 +0400

    added constants

---
+++ src/constants/email-token.constants.js
@@ -0,0 +1,5 @@
+const EMAIL_TOKEN_TTL_MS = 3 * 60 * 60 * 1000;
+
+module.exports = {
+    EMAIL_TOKEN_TTL_MS,
+};
---
+++ src/constants/url.constants.js
@@ -0,0 +1,5 @@
+const ACTIVATION_CONFIRMATION_PATH = "/email-confirmation";
+
+module.exports = {
+    ACTIVATION_CONFIRMATION_PATH,
+};
