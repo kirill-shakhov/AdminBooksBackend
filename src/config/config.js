cf19001f87f1de0ea4b2771bdf3746e9612d5df7
Kirill <checkster98@gmail.com>
2026-05-05 11:19:50 +0400

    updated config

--- src/config/config.js
+++ src/config/config.js
@@ -1,4 +1,13 @@
+const isProduction = process.env.NODE_ENV === "production";
+
+const clientUrl = isProduction
+    ? process.env.CLIENT_URL_PROD 
+    : process.env.CLIENT_URL_LOCAL
+    
 module.exports = {
     secret: "SECRET_KEY_RANDOM",
-    googleClientId: process.env.GOOGLE_CLIENT_ID || ""
+    googleClientId: process.env.GOOGLE_CLIENT_ID || "",
+    resendApiKey: process.env.RESEND_API_KEY || "",
+    resendFrom: process.env.RESEND_FROM || "onboarding@resend.dev",
+    clientUrl,
 }
